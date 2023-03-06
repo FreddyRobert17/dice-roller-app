93e89d75494c0160b147f225e86b94660aac968b
Freddy-1703 <freddy.bedregal.t@gmail.com>
2023-03-06 10:02:15 -0500

    First commit

---
+++ app/src/main/java/com/app/composeapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.app.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerApp()
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)){
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        var result by remember { mutableStateOf(1) }
+
+        val imageResource = when(result){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+        Button(onClick = { result = (1..6).random() }) {
+          Text(stringResource(id = R.string.roll))
+        }
+    }
+}
+
+@Preview(showSystemUi=true)
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage()
+}
